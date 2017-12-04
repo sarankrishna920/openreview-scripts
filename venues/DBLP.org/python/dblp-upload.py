0f325511d8fd3a21457d1ed5ef6866be9049283f
Melisa Bok <melisabok@gmail.com>
2017-12-04 16:18:01 -0500

    add multiprocessing

--- venues/DBLP.org/python/dblp-upload.py
+++ venues/DBLP.org/python/dblp-upload.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 from xml.etree  import ElementTree
 from openreview import *
 import config
+from multiprocessing import Pool
 
 
 parser = argparse.ArgumentParser()
@@ -24,16 +25,19 @@ context = ET.iterparse(args.file, events = ('end', ), parser=parser)
 
 count = 0
 
+def post_note(elem):
+	note = openreview.Note(content = { 'dblp': elem }, invitation = config.SUBMISSION, readers = ['everyone'], writers = [config.CONF], signatures = [config.CONF])
+	saved_note = client.post_note(note)
+	print saved_note.id
+
+p = Pool(10)
+elem_buffer = []
 for event, elem in context:
-    if elem.tag in ['article', 'inproceedings', 'proceedings', 'book', 'incollection', 'phdthesis', 'mastersthesis', 'www', 'person', 'data']:
 
-    	note = openreview.Note(content = { 'dblp': ElementTree.tostring(elem, encoding='utf8', method='xml') }, invitation = config.SUBMISSION, readers = ['everyone'], writers = [config.CONF], signatures = [config.CONF])
+	if elem.tag in ['article', 'inproceedings', 'proceedings', 'book', 'incollection', 'phdthesis', 'mastersthesis', 'www', 'person', 'data']:
+		elem_buffer.append(ElementTree.tostring(elem, encoding='utf8', method='xml'))
 
-        saved_note = client.post_note(note)
-
-        print saved_note.id
-
-        count = count + 1
-
-        if count > 10:
-        	break
+		#post_note(ElementTree.tostring(elem, encoding='utf8', method='xml'))
+		if len(elem_buffer) > 100000:
+			p.map(post_note, elem_buffer[:])
+			elem_buffer = []
