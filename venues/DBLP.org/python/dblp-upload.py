337f47834bfee93f9ea86aa1d35c48a2b9858f01
Melisa Bok <melisabok@gmail.com>
2018-03-05 11:50:47 -0500

    add post with retries

--- venues/DBLP.org/python/dblp-upload.py
+++ venues/DBLP.org/python/dblp-upload.py
@@ -25,21 +25,36 @@ context = ET.iterparse(args.file, events = ('end', ), parser=parser)
 
 count = 0
 
-def post_note(elem):
-	print elem.get('key')
-	note = openreview.Note(content = { 'dblp': ElementTree.tostring(elem, encoding='utf8', method='xml') }, invitation = config.SUBMISSION, readers = ['everyone'], writers = [config.CONF], signatures = [config.CONF])
-	saved_note = client.post_note(note)
-	print elem.get('key') + ',' + saved_note.id
+def post_with_retries(note):
+	response = None
+	count = 0
+	while response is None and count < 5:
+		try:
+			response = client.post_note(note)
+		except requests.exceptions.ConnectionError:
+			count += 1
 
-p = Pool(10)
+	if not response:
+		print 'Could not post the note', note.content.dblp
+
+	return response
+
+def post_note(elem):
+	note = openreview.Note(content = { 'dblp': ElementTree.tostring(elem, encoding='utf8', method='xml') }, invitation = config.SUBMISSION, readers = ['everyone'], writers = [config.CONF], signatures = [config.CONF])
+	saved_note = post_with_retries(note)
+	if saved_note.invitation != config.SUBMISSION:
+		print 'Existent submission', saved_note.id
+	else
+		print elem.get('key') + ',' + saved_note.id
+
+p = Pool(4)
 elem_buffer = []
 for event, elem in context:
 
-	if elem.tag in ['article', 'inproceedings', 'proceedings', 'book', 'incollection', 'phdthesis', 'mastersthesis', 'www', 'person', 'data']:
+	if elem.tag in ['article', 'inproceedings', 'proceedings', 'book', 'incollection', 'phdthesis', 'mastersthesis', 'person', 'data']:
 
 		elem_buffer.append(elem)
 
-		#post_note(ElementTree.tostring(elem, encoding='utf8', method='xml'))
-		if len(elem_buffer) > 100000:
+		if len(elem_buffer) > 1000:
 			p.map(post_note, elem_buffer[:])
 			elem_buffer = []
